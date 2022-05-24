b7842936d0b6b876f24d3e809f052c48312acbc2
Bhavuk <bhavuksharma2202@gmail.com>
2022-05-24 10:43:53 +0530

    changed input box design

--- app/src/main/java/com/redheadhammer/minimaltodolist/MainActivity.java
+++ app/src/main/java/com/redheadhammer/minimaltodolist/MainActivity.java
@@ -1,9 +1,5 @@
 package com.redheadhammer.minimaltodolist;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,6 +7,9 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
@@ -41,20 +40,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void save(View view) {
         String itemName = String.valueOf(item.getText());
-        items.add(itemName);
-        arrayAdapter.notifyDataSetChanged();
-        item.setText("");
-        FileHelper.writeData(items, getApplicationContext());
+        if (itemName.length() > 0) {
+            items.add(itemName);
+            arrayAdapter.notifyDataSetChanged();
+            item.setText("");
+            FileHelper.writeData(items, getApplicationContext());
+        }
     }
 
-    public void remove(AdapterView<?> adapterView, View view, int i , long l){
+    public void remove(AdapterView<?> adapterView, View view, int i, long l) {
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setTitle("Delete Item");
         alert.setMessage("Do you want to delete the item?");
         alert.setPositiveButton("Yes", (dialogInterface, k) -> {
             items.remove(i);
             arrayAdapter.notifyDataSetChanged();
-            FileHelper.writeData( items, getApplicationContext());
+            FileHelper.writeData(items, getApplicationContext());
         });
         alert.setNegativeButton("No", (dialogInterface, m) -> dialogInterface.cancel());
 
@@ -62,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
-    private void writeItems(){
+    private void writeItems() {
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.activity_list_item,
                 android.R.id.text1, items);
         list.setAdapter(arrayAdapter);
